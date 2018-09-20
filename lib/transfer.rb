a1561182a9c35f54a02a9c7bd07ab205ce2b92d4
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-09-20 16:05:31 -0400

    completed! teamwork! patience! :taco:

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :amount
+  attr_writer
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @status == "pending" && @sender.valid? && @amount <= @sender.balance
+      @sender.balance-= @amount
+      @receiver.balance+= @amount
+      @status= "complete"
+    elsif @status == "pending"
+      @status= "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance+= @amount
+      @receiver.balance-= @amount
+      @status= "reversed"
+    end 
+  end
+
 end
